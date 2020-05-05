882bce0c6dfb84a410983bd53f970265ceba7354
belaustegui <cristian.alvb@gmail.com>
2020-05-05 18:20:29 +0200

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 # Utilities
 sudo apt install -y \
-  stow zsh awscli vim code imagemagick gnome-tweaks flameshot google-chrome-stable \
-  flatpak docker.io docker-compose
+  stow zsh awscli vim imagemagick gnome-tweaks docker.io docker-compose code google-chrome-stable
 
 # Set ZSH as default shell
 chsh -s /usr/bin/zsh
@@ -9,10 +8,7 @@ chsh -s /usr/bin/zsh
 sudo gpasswd -a crbelaus docker
 
 # Development dependencies
-sudo apt install -y \
-  automake autoconf libreadline-dev libncurses-dev libssl-dev libyaml-dev \
-  libxslt1-dev libffi-dev libtool unixodbc-dev libwxgtk3.0-gtk3-dev \
-  libgl1-mesa-dev libglu1-mesa-dev libpng-dev inotify-tools
+sudo apt install -y automake autoconf libncurses5-dev libssl-dev inotify-tools
 
 # Setup flathub.org for flatpaks
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
@@ -30,13 +26,14 @@ flatpak install -y flathub \
   org.gimp.GIMP \
   org.gnome.Boxes \
   org.telegram.desktop \
-  org.videolan.VLC
-
+  org.videolan.VLC \
+  com.github.johnfactotum.Foliate \
+  org.gnome.gitlab.somas.Apostrophe
 
 # Set up ASDF Version Manager from asdf-vm.com
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.8
-echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
-echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
+echo -e '. $HOME/.asdf/asdf.sh' >> ~/.bashrc
+echo -e '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
 # Add language plugins
 asdf plugin-add nodejs && asdf plugin-add erlang && asdf plugin-add elixir
 # Import NodeJS release keys (https://github.com/asdf-vm/asdf-nodejs)
